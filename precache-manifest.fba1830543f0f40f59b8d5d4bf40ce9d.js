5ece458703a96de41bb90766cedb851fce03be99
KostousovaNA <n.kostousova@rusbitech.ru>
2020-10-17 16:08:34 +0300

    Updates

---
+++ precache-manifest.fba1830543f0f40f59b8d5d4bf40ce9d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7bee25ea45ca1b3b1366aec22e68f3e",
+    "url": "/simple-react-homeworks/index.html"
+  },
+  {
+    "revision": "1ac9af0ea21c40f4f308",
+    "url": "/simple-react-homeworks/static/css/main.eb89c5f2.chunk.css"
+  },
+  {
+    "revision": "6761ebb64dec27bc0e72",
+    "url": "/simple-react-homeworks/static/js/2.f5386415.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/simple-react-homeworks/static/js/2.f5386415.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ac9af0ea21c40f4f308",
+    "url": "/simple-react-homeworks/static/js/main.70a79e33.chunk.js"
+  },
+  {
+    "revision": "af4ec8013c5fe87b14da",
+    "url": "/simple-react-homeworks/static/js/runtime-main.f59815ab.js"
+  },
+  {
+    "revision": "6c2ed262984760bae1308566d7e4e166",
+    "url": "/simple-react-homeworks/static/media/kote.6c2ed262.png"
+  }
+]);
